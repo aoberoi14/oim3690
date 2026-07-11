b02c257f7bf9676e5e0b661e48cbfadb9c05b553
aoberoi14 <aoberoi1@babson.edu>
2026-07-11 18:33:07 -0400

    adding data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+// Data lives here, separate from the rendering logic in main.js.
+export const books = [
+  { title: "Atomic Habits", author: "James Clear", rating: 4.8 },
+  { title: "The Lean Startup", author: "Eric Ries", rating: 4.4 },
+  { title: "Shoe Dog", author: "Phil Knight", rating: 4.9 },
+  { title: "Zero to One", author: "Peter Thiel", rating: 4.5 },
+  { title: "Sapiens", author: "Yuval Noah Harari", rating: 4.7 },
+];
